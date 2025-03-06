cbf7c2a0439847778e28b73dc8ceed491556cc17
m4ferreirac <147309579+m4ferreirac@users.noreply.github.com>
2025-03-06 08:48:03 +0000

    Update Initial Commit

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ const toggleNav = () => {
   if (nav.classList.contains("hidden")) {
     btnToggleNav.textContent = "menu";
   } else {
-    // When menu is opened after transition change text respectively
     setTimeout(() => {
       btnToggleNav.textContent = "close";
     }, 475);
